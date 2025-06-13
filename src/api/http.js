32c8056b52da044ff8aa1777591730e515333dc6
evghn <evghn94_git@mail.ru>
2025-06-13 21:19:18 +0300

    fix axios local

--- src/api/http.js
+++ src/api/http.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 
-const API_BASE_URL = "/api";
+export const API_BASE_URL = "/api";
 export const API_URLS = {
   login: `${API_BASE_URL}/login`,
-  tasks: `${API_BASE_URL}/tasks`,
+  tasks: `${API_BASE_URL}/login`,
   edit: `${API_BASE_URL}/tasks/`,
   login: `${API_BASE_URL}/login`,
 };
 
 export const http = axios.create({
   baseURL: API_BASE_URL,
-  baseURL: import.meta.env.DEV
-    ? API_BASE_URL // будет использовать прокси в development
-    : "https://max220mm.beget.tech", // прямое обращение в production
   timeout: 10000,
   headers: {},
 });
