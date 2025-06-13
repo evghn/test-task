bfb73f5e34a9bf4ae5d148763d57434d0d30a22d
evghn <evghn94_git@mail.ru>
2025-06-13 21:15:47 +0300

    fix axios proxy

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://max220mm.beget.tech/api";
+const API_BASE_URL = "/api";
 export const API_URLS = {
   login: `${API_BASE_URL}/login`,
   tasks: `${API_BASE_URL}/tasks`,
@@ -10,6 +10,9 @@ export const API_URLS = {
 
 export const http = axios.create({
   baseURL: API_BASE_URL,
+  baseURL: import.meta.env.DEV
+    ? API_BASE_URL // будет использовать прокси в development
+    : "https://max220mm.beget.tech", // прямое обращение в production
   timeout: 10000,
   headers: {},
 });
